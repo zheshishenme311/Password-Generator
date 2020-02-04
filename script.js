391e4791ecb5287e89fac83716a21a117bb4f3d1
Bowen Yang <bowenyang@Bowens-MacBook-Pro.local>
2020-02-04 10:03:52 -0500

    first commit

--- script.js
+++ script.js
@@ -1,14 +1,60 @@
 // Assignment Code
+var lowerCase, upperCase, numberCase, specialCase, myLength;
 var generateBtn = document.querySelector("#generate");
+var cpyButton = document.querySelector("#cpyButton");
+
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  lowerCase = document.getElementById("lowerCase").checked;
+  upperCase = document.getElementById('upperCase').checked;
+  numberCase = document.getElementById('numberCase').checked;
+  specialCase = document.getElementById('specialCase').checked;
+  mylength = document.getElementById('mylength').value;
+  
+  var myPasswordCharacters = '';
+  if( lowerCase )
+    myPasswordCharacters += "abcdefghijklmnopqrstuvwxyz";
+  if( upperCase )
+    myPasswordCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if( numberCase )
+    myPasswordCharacters += "0123456789";
+  if( specialCase )
+    myPasswordCharacters += "~!@#$%^&*()_+";
 
-  passwordText.value = password;
+  console.log(` mylength='${mylength}' ${myPasswordCharacters}` );
 
+  var newPassword = '';
+  for( var i=0; i<mylength; i++ ){
+    // randomly pick a character and add to the new password
+    var newCharIdx = Math.floor( Math.random()*myPasswordCharacters.length );
+    newPassword += myPasswordCharacters[newCharIdx];
+  }
+
+  console.log( ` .. generated password: ${newPassword}` );
+  // var password = generatePassword();
+  document.querySelector("#password").innerHTML = newPassword; 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+console.log(cpyButton);
+cpyButton.addEventListener("click", myFunction);
+
+function myFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("password");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+  console.log("ssss");
+
+  /* Alert the copied text */
+  //alert("Copied the text: " + copyText.value);
+}
